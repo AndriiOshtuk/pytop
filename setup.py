242578532e02cf22e971cef0a556cfe8bbb3bb69
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-01-09 12:07:41 +0200

    Version 2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+#TODO(AOS) Add missing fields
+setup(
+    name="pytop",
+    version='0.0.1 Alpha 1',
+    description='Htop copycat implemented in Python.',
+    author='Andrii Oshtuk',
+    url='https://github.com/AndriiOshtuk/pytop',
+    license='MIT',
+    platforms='Linux',
+)
