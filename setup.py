14478223c79bbde4ea16eae8f83a4d6b40fc445f
Eric Cousineau <eric.cousineau@tri.global>
2021-05-29 20:42:04 -0400

    setup: Add console script

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     name="pytop",
     version='1.0.0',
     packages=['pytop'],
+    entry_points={
+        "console_scripts": [
+            "pytop=pytop:main",
+        ],
+    },
     install_requires=[
         'urwid>=2.1.0',
     ],
